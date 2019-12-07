6d401d061871abee2daef9a409820d4c5dc04127
dczysz <32844299+dczysz@users.noreply.github.com>
2019-12-06 17:49:46 -0700

    Implemented basic state machine

---
+++ src/fetchMachine.js
@@ -0,0 +1,47 @@
+// https://xstate.js.org/docs/tutorials/reddit.html#async-flow
+// https://codesandbox.io/embed/33wr94qv1
+
+import { Machine, assign } from 'xstate';
+import axios from 'axios';
+
+const fetchWeather = context => {
+  const { query } = context;
+
+  return axios.get('/weather/' + query).then(res => res.data);
+};
+
+const fetchMachine = Machine({
+  id: 'fetch',
+  initial: 'loading',
+  context: {
+    query: 'chicago, il',
+    weather: null,
+  },
+  states: {
+    idle: {
+      on: {
+        CHANGE: {
+          actions: assign({
+            query: (_ctx, e) => e.value,
+          }),
+        },
+        FETCH: 'loading',
+      },
+    },
+    loading: {
+      invoke: {
+        id: 'fetch-weather',
+        src: fetchWeather,
+        onDone: {
+          target: 'idle',
+          actions: assign({
+            weather: (_ctx, e) => e.data,
+          }),
+        },
+        onError: 'idle',
+      },
+    },
+  },
+});
+
+export default fetchMachine;

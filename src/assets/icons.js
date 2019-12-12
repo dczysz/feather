d97fc57b5741ae0b2fd51e83bb99f2d0be879bb2
dczysz <32844299+dczysz@users.noreply.github.com>
2019-12-12 16:39:48 -0700

    Added Icons

---
+++ src/assets/icons.js
@@ -0,0 +1,29 @@
+import { ReactComponent as clearDay } from './Sun.svg';
+import { ReactComponent as clearNight } from './Moon.svg';
+import { ReactComponent as rain } from './Cloud-Rain.svg';
+import { ReactComponent as snow } from './Cloud-Snow.svg';
+import { ReactComponent as sleet } from './Cloud-Hail.svg';
+import { ReactComponent as hail } from './Cloud-Hail-Alt.svg';
+import { ReactComponent as wind } from './Wind.svg';
+import { ReactComponent as fog } from './Cloud-Fog-Alt.svg';
+import { ReactComponent as cloudy } from './Cloud.svg';
+import { ReactComponent as partlyCloudyDay } from './Cloud-Sun.svg';
+import { ReactComponent as partlyCloudyNight } from './Cloud-Moon.svg';
+import { ReactComponent as thunderstorm } from './Cloud-Lightning.svg';
+import { ReactComponent as tornado } from './Tornado.svg';
+
+export default {
+  'clear-day': clearDay,
+  'clear-night': clearNight,
+  'partly-cloudy-day': partlyCloudyDay,
+  'partly-cloudy-night': partlyCloudyNight,
+  cloudy,
+  fog,
+  hail,
+  rain,
+  sleet,
+  snow,
+  thunderstorm,
+  tornado,
+  wind,
+};
